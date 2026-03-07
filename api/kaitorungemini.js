7086fa4b1c3692a4cbac9a2bf346a6694396e076
Camyro Braga <k1m1r0@proton.me>
2026-03-07 17:29:30 -0400

    3.3
    correção de erros

--- api/kaitorungemini.js
+++ api/kaitorungemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY_FOR_KAITO_RUN);
 
 async function chamarAPI(prompt) {
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
     const result = await model.generateContent(prompt);
     return result.response.text().trim();
 }
